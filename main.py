dfbdb703b0476cb2707c63f627d556ad6f92cdba
purin52002 <purin7635@gmail.com>
2018-12-10 15:50:03 +0900

    consuder stack_num

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random_info_maker import make_random_info_list
 from comparison_maker import make_comparison_list
     vec_obs_size=1
     act_size=1
-    info_list = random_info_list_make(10, vec_obs_size, act_size)
-    info_list = make_random_info_list(10, vec_obs_size, act_size)
+    stack_num = 1
+    info_list = make_random_info_list(10, vec_obs_size, act_size, stack_num)
     comparison_list = make_comparison_list(10,info_list)
+    predict_model = PredictModel(sess, vec_obs_size, act_size, stack_num, scope='', layer_num=3)
--- random_info_maker.py
+++ random_info_maker.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def make_random_info(vec_obs_size:int, act_size:int):
-    observation = 2*np.random.rand(vec_obs_size)-1
-    action = 2*np.random.rand(act_size)-1
-    reward = 2*np.random.rand()-1
+def make_random_info(vec_obs_size:int, act_size:int, stack_num:int):
+    observation = [2*np.random.rand(vec_obs_size)-1 for x in range(stack_num)]
+    action = [2*np.random.rand(act_size)-1 for x in range(stack_num)]
+    reward = [2*np.random.rand()-1 for x in range(stack_num)]
     return {
         'observation':observation,
         'action':action,
@@ -12,5 +12,5 @@ def make_random_info(vec_obs_size:int, act_size:int):
     
 
 
-def make_random_info_list(data_num:int, vec_obs_size:int, act_size:int):
-    return [make_random_info(vec_obs_size, act_size) for x in range(data_num)]
+def make_random_info_list(data_num:int, vec_obs_size:int, act_size:int, stack_num:int):
+    return [make_random_info(vec_obs_size, act_size, stack_num) for x in range(data_num)]
