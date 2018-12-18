25439b717b8f74b4e6892bcf45f83b75f8b03b17
purin52002 <purin7635@gmail.com>
2018-12-18 23:19:34 +0900

    add class ComparisonMaker

--- my_rl_teacher/__init__.py
+++ my_rl_teacher/__init__.py
@@ -1,2 +1,3 @@
 from .comparison_collectors import *
 from .predict_model import *
+from .comparison_maker import *
--- my_rl_teacher/comparison_maker.py
+++ my_rl_teacher/comparison_maker.py
@@ -1,27 +1,39 @@
 import numpy as np
 
-def make_comparison(left_info:dict, right_info:dict, use_score:bool, score_func):
-    return {
-        'left':left_info,
-        'right':right_info,
-        'score' if use_score else 'label':score_func(left_info,right_info)
-    }
+class ComparisonMaker:
+    def __init__(self, comparison_num:int, info_list:list, use_score:bool, batch_size:int, label_func):
+        self.comparison_num = comparison_num
+        self.batch_size = batch_size
 
-def choice_left_right_info(info_list:list):
-    choice_info = np.random.choice(info_list, 2, False)
-    return {
-        'left_info':choice_info[0],
-        'right_info':choice_info[1]
-    }
+        assert self.comparison_num >= self.batch_size, 'comparison_num is lesser than batch_size'
 
-def make_comparison_list(comparison_num:int ,info_list:list, use_score:bool, score_func):
-    comparison_list = [None]*comparison_num
-    for i in range(comparison_num):
-        choice_info = choice_left_right_info(info_list)
-        comparison_list[i] = make_comparison(choice_info['left_info'], choice_info['right_info'], use_score, score_func)
+        self.comparison_list = self.make_comparison_list(comparison_num, info_list, use_score, label_func)
 
-    return comparison_list
+    def make_comparison(self, left_info:dict, right_info:dict, use_score:bool, score_func):
+        return {
+            'left':left_info,
+            'right':right_info,
+            'score' if use_score else 'label':score_func(left_info,right_info)
+        }
 
-    
+    def choice_left_right_info(self, info_list:list):
+        choice_info = np.random.choice(info_list, 2, False)
+        return {
+            'left_info':choice_info[0],
+            'right_info':choice_info[1]
+        }
+
+    def make_comparison_list(self, comparison_num:int ,info_list:list, use_score:bool, score_func):
+        comparison_list = [None]*comparison_num
+        for i in range(comparison_num):
+            choice_info = self.choice_left_right_info(info_list)
+            comparison_list[i] = self.make_comparison(choice_info['left_info'], choice_info['right_info'], use_score, score_func)
+
+        return comparison_list
+
+    def make_comparison_batch(self):
+        return np.random.choice(self.comparison_list, self.batch_size, False)
+
+
 
 
