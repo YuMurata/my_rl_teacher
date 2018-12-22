fedb7595dbf7ddf0a85623bdb2eeb4cf3692b467
purin52002 <purin7635@gmail.com>
2018-12-22 14:17:08 +0900

    add partial_model

--- my_rl_teacher/partial_model.py
+++ my_rl_teacher/partial_model.py
@@ -1,17 +1,43 @@
 import yaml
 import json
 from pprint import pprint
-from my_rl_teacher.predict_model import PredictModel
-
+from my_rl_teacher.predict_model import PredictEvaluationModel
+import tensorflow as tf
+import numpy as np
 class PartialPredictModel:
-    def __init__(self, structure_file:str):
+    def __init__(self, structure_file:str, use_score:bool,scope:str, summary_writer:tf.summary.FileWriter):
         with open(structure_file,'r') as f:
-            structure = json.load(f)
-        pprint(data)
-        print('partial: {0}'.format(len(data.items())))
+            self.structure = json.load(f)
+        pprint(self.structure)
+        print('partial: {0}'.format(len(self.structure['partial'].items())))
         self.predict_models = {}
 
-        for partial_name, index_list in data.items():
-            self.predict_models[partial_name]=PredictModel()
+        with tf.variable_scope(scope):
+            for partial_name, index_list in self.structure['partial'].items():
+                self.predict_models[partial_name]=PredictEvaluationModel(len(index_list), self.structure['stack_num'], partial_name, 3, use_score, summary_writer)
+                import pdb;pdb.set_trace()
+
+    def predict_reward(self, segment_list):
+        reward_list = []
+        for partial_name, index_list in self.structure['partial'].items():
+            reward_list.append(self.predict_models[partial_name].predict_reward(segment_list[index_list]))
+
+        return sum(reward_list)
+
+    def update_model(self,labeled_comparisons_batch):
+        loss_list = []
+        for partial_name, index_list in self.structure['partial'].items():
+            partial_comparison_batch =[{
+                'left':{'parameter':comp['left']['parameter'][index_list]},
+                'right':{'parameter':comp['right']['parameter'][index_list]},
+                'score':comp['score'][partial_name]
+                }
+                for comp in labeled_comparisons_batch
+            ]
+
+            loss_list.append(self.predict_models[partial_name].update_model(partial_comparison_batch))
+
+        return np.mean(loss_list)
 
 if __name__ == "__main__":
+    partial_model = PartialPredictModel('tests/test.json',True, tf.summary.FileWriter('summary'))
